6b0010397a0ef792e81515c444f44bb2ceafb5a5
savalet <savinien.petitjean@gmail.com>
2025-02-03 17:47:23 +0100

    Remove unused function in ulib

--- ulib/u_mem.h
+++ ulib/u_mem.h
@@ -11,11 +11,8 @@
     #include <stdint.h>
     #define IDX_OF(array, i, mem_s) (array + ((i) * (mem_s)))
 
-int u_get_array_size(char **);
 void *u_memcpy(uint8_t *, uint8_t const *, size_t);
 void *u_realloc(void *, size_t, size_t);
 void u_swap(int *, int *);
-void u_qsort(char *, size_t, size_t,
-    int compare(void const *, void const *));
 
 #endif /* MEM_H */
--- ulib/u_str.h
+++ ulib/u_str.h
@@ -12,11 +12,6 @@ int u_getnbr(char const *);
 char *u_strcat(char *, char const *);
 char *u_strncat(char *, char const *, int);
 char *u_strdup(char const *);
-char *u_strpop(char *, int);
-int u_char_isalpha(char);
-int u_char_isnum(char);
-int u_str_isalphanum(char *);
-void u_freestr(char **);
 int u_strlen(char const *);
 char *u_strcpy(char *, char const *);
 char *u_strncpy(char *, char const *, int);
@@ -24,14 +19,6 @@ char *u_revstr(char *);
 char *u_strstr(char *, char const *);
 int u_strcmp(char const *, char const *);
 int u_strncmp(char const *, char const *, int);
-char *u_strupcase(char *);
-char *u_strlowcase(char *);
-char *u_strcapitalize(char *);
-int u_str_isalpha(char *);
-int u_str_isnum(char *);
-int u_str_islower(char *);
-int u_str_isupper(char *);
-int u_str_isprintable(char *);
 char *u_numstr(char *, int);
 
 #endif /* STRING_H */
