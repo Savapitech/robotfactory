6a87dd8bffcec764a05a33fa9484f01c20977e55
savalet <savinien.petitjean@gmail.com>
2025-02-25 18:02:34 +0100

    Add label parsing

--- src/common.h
+++ src/common.h
@@ -10,6 +10,8 @@
     #include <ctype.h>
     #include <stddef.h>
 
+    #include "u_str.h"
+
     #define WRITE_CONST(fd, str) write(fd, str, sizeof str - 1)
     #define DEFAULT_LINES_CAP 8
     #define SKIP_SPACES(buff) while (isspace(*buff) && *buff != '\0') buff++
@@ -20,10 +22,20 @@ enum {
 };
 
 typedef struct {
+    buff_t name;
+    char *ptr;
+} label_t;
+
+typedef struct {
+    // Buffer
     char **lines;
     size_t lines_sz;
     size_t lines_cap;
     size_t lines_i;
+    // Compiler
+    label_t *lbl_table;
+    size_t lbl_table_sz;
+    // Other
     char const *file_name;
 } rf_t;
 
--- src/label.c
+++ src/label.c
@@ -6,6 +6,7 @@
 */
 
 #include <stdbool.h>
+#include <stdlib.h>
 
 #include "compiler.h"
 #include "common.h"
@@ -26,16 +27,39 @@ size_t get_lbl_sz(char *line)
     return lbl_size;
 }
 
-bool parse_label_table(rf_t *rf)
+static
+size_t get_lbl_count(rf_t *rf)
 {
     int lbl_size;
     size_t lbl_count = 0;
 
     for (size_t i = rf->lines_i; i < rf->lines_sz; i++) {
         lbl_size = get_lbl_sz(rf->lines[i]);
+        if (lbl_size)
+            lbl_count++;
+    }
+    return lbl_count;
+}
+
+bool parse_label_table(rf_t *rf)
+{
+    int lbl_size;
+    size_t lbl_i = 0;
+
+    rf->lbl_table_sz = get_lbl_count(rf);
+    if (rf->lbl_table_sz < 1)
+        return false;
+    rf->lbl_table = malloc(sizeof *rf->lbl_table * rf->lbl_table_sz);
+    if (rf->lbl_table == NULL)
+        return false;
+    for (size_t i = rf->lines_i; i < rf->lines_sz; i++) {
+        lbl_size = get_lbl_sz(rf->lines[i]);
         if (lbl_size) {
-            U_DEBUG("Label found [%d] [%.*s]\n", lbl_size, lbl_size,
-                rf->lines[i]);
+            rf->lbl_table[lbl_i].name.str = rf->lines[i];
+            rf->lbl_table[lbl_i].name.sz = lbl_size;
+            U_DEBUG("Label [%lu] found [%d] [%.*s]\n", lbl_i, lbl_size,
+                lbl_size, rf->lines[i]);
+            lbl_i++;
         }
     }
     return true;
