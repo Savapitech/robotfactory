9ac53d618e8e49a85840f26252e2cb21b378d0af
savalet <savinien.petitjean@gmail.com>
2025-02-27 15:41:18 +0100

    Get header now write directly in the final header to reduce the malloc count

--- src/compiler.c
+++ src/compiler.c
@@ -19,32 +19,31 @@
 #include "u_mem.h"
 
 static
-char *get_header(rf_t *rf)
+bool write_header(rf_t *rf)
 {
     size_t header_sz = PROG_NAME_LENGTH + STRUCT_PADDING + COMMENT_LENGTH;
-    char *header = malloc(sizeof(char) * header_sz);
+    char *header_str = rf->final_buff.str;
     buff_t name;
     buff_t comment;
 
-    if (header == NULL)
-        return NULL;
-    u_bzero(header, header_sz);
-    *(uint32_t *)(header) = swap_uint32(COREWAR_EXEC_MAGIC);
-    header += sizeof(int);
+    u_bzero(header_str, header_sz);
+    *(uint32_t *)(header_str) = swap_uint32(COREWAR_EXEC_MAGIC);
+    header_str += sizeof(int);
     name = get_metadata(rf->lines[rf->lines_i], NAME_CMD_STRING);
     rf->lines_i++;
     comment = get_metadata(rf->lines[rf->lines_i], COMMENT_CMD_STRING);
     rf->lines_i++;
     if (name.str == NULL || comment.str == NULL)
-        return NULL;
-    u_strncpy(header, name.str, name.sz);
-    header += header_sz - COMMENT_LENGTH;
-    u_strncpy(header, comment.str, comment.sz);
-    return header - (header_sz - COMMENT_LENGTH) - sizeof(int);
+        return false;
+    u_strncpy(header_str, name.str, name.sz);
+    header_str += header_sz - COMMENT_LENGTH;
+    u_strncpy(header_str, comment.str, comment.sz);
+    rf->final_buff.sz = header_sz;
+    return true;
 }
 
 static
-bool write_in_file(rf_t *rf, char *header)
+bool write_in_file(rf_t *rf)
 {
     char new_file_name[u_strlen(rf->file_name) + 2];
     char const *file_name = rf->file_name;
@@ -57,17 +56,17 @@ bool write_in_file(rf_t *rf, char *header)
     U_DEBUG("Writing in file [%s]\n", new_file_name);
     if (new_file_fd == -1)
         return (WRITE_CONST(STDERR_FILENO, "Cannot write in file !\n"), false);
-    write(new_file_fd, header, sizeof(char) * (PROG_NAME_LENGTH +
-            COMMENT_LENGTH));
+    write(new_file_fd, rf->final_buff.str, rf->final_buff.sz);
     return (close(new_file_fd), true);
 }
 
 __attribute__((nonnull(1)))
 bool prepare_compilation(rf_t *rf)
 {
-    char *padded_header = get_header(rf);
-
+    if (!write_header(rf))
+        return (WRITE_CONST(STDERR_FILENO, CYAN "Cannot parse header.\n"
+            RESET), false);
     parse_label_table(rf);
     process_instructions(rf);
-    return write_in_file(rf, padded_header);
+    return write_in_file(rf);
 }
