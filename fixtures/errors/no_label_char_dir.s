3bc93800c66ff02fa476eb3912776b20382d04ed
Alice Duvillier <alice.duvillier@epitech.eu>
2025-03-07 15:27:43 +0100

    Adding files test, trying label without ':'

---
+++ fixtures/errors/no_label_char_dir.s
@@ -0,0 +1,5 @@
+.name "No label char after direct"
+.comment "no label char after direct"
+
+hi:
+	sti r1, %hi, %1
---
+++ fixtures/errors/no_val_dir.s
@@ -0,0 +1,4 @@
+.name "No val after direct"
+.comment "no val after direct"
+
+	sti r1, %, %1
